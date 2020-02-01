a3d168dc39ee597c6e3ce57e8a592ebe45d0decc
Natesh Narain <nnaraindev@gmail.com>
2020-02-01 10:19:27 -0500

    Resolves #21. Benchmarks for emulate frame

---
+++ nescore/benches/bench_nes.rs
@@ -0,0 +1,14 @@
+#![feature(test)]
+
+extern crate test;
+
+use test::Bencher;
+use nescore::{Nes, Cartridge};
+
+#[bench]
+fn bench_emulate_frame(b: &mut Bencher) {
+    let cart = Cartridge::from_path("tests/roms/nestest.nes").unwrap();
+    let mut nes = Nes::new().with_cart(cart);
+
+    b.iter(|| nes.emulate_frame());
+}
