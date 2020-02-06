dc3fd9043b70183178ed58befb70deb98b8ffaef
Natesh Narain <nnaraindev@gmail.com>
2020-02-05 22:33:30 -0500

    Benchmarks in Github Actions

--- nescore/benches/bench_nes.rs
+++ nescore/benches/bench_nes.rs
@@ -7,7 +7,7 @@ use nescore::{Nes, Cartridge};
 
 #[bench]
 fn bench_emulate_frame(b: &mut Bencher) {
-    let cart = Cartridge::from_path("tests/roms/nestest.nes").unwrap();
+    let cart = Cartridge::from_path("tests/roms/nestest/nestest.nes").unwrap();
     let mut nes = Nes::new().with_cart(cart);
 
     b.iter(|| nes.emulate_frame());
