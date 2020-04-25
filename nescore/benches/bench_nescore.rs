d0c6db3938910e91c34f173327d933caa4b5ec05
Natesh Narain <nnaraindev@gmail.com>
2020-04-25 01:18:41 +0000

    Resolves #35. Criterion benchmark for emulate frame

--- nescore/benches/bench_nescore.rs
+++ nescore/benches/bench_nescore.rs
@@ -1,29 +1,16 @@
 use criterion::{criterion_main, criterion_group, Criterion};
-
 use nescore::{Nes, Cartridge};
 
 fn bench_emulate_frame(c: &mut Criterion) {
-    let mut nes = Cartridge::from_path("tests/roms/nestest/nestest.nes")
-                        .map(|cart| Nes::default().with_cart(cart)).unwrap();
-    c.bench_function("Nes::emulate_frame()", |b| b.iter(|| nes.emulate_frame()));
+    let cart = Cartridge::from_path("tests/roms/nestest/nestest.nes").unwrap();
+    let mut nes = Nes::default().with_cart(cart);
+
+    c.bench_function("Nes::emulate_frame()", |b| {
+        b.iter(|| {
+            nes.emulate_frame();
+        });
+    });
 }
 
 criterion_group!(benches, bench_emulate_frame);
 criterion_main!(benches);
-
-// #[bench]
-// fn bench_emulate_frame(b: &mut Bencher) {
-//     let mut nes = Cartridge::from_path("tests/roms/nestest/nestest.nes")
-//                         .map(|cart| Nes::default().with_cart(cart)).unwrap();
-
-//     b.iter(|| nes.emulate_frame());
-// }
-
-// #[bench]
-// fn bench_nestest(b: &mut Bencher) {
-//     let mut nes = Cartridge::from_path("tests/roms/nestest/nestest.nes")
-//                         .map(|cart| Nes::default().with_cart(cart).entry(0xC000)).unwrap();
-
-//     // TODO: The PC needs to be reset before running again
-//     b.iter(|| nes.run_until(0xC66E));
-// }
